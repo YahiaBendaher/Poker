ca2e187078a57da156d13064f7ecd7953c93ffef
Ahmed Belaid <elyesbelaid.2005@gmail.com>
2025-10-17 17:10:42 +0200

    Scanner

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Main 1 : ");
+        String main1 = scanner.nextLine();
+        System.out.print(main1);
+    }
+}
