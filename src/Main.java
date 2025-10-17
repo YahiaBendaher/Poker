e6354d7463495950af7b1fe8fff0671d59a35fb4
Ahmed Belaid <elyesbelaid.2005@gmail.com>
2025-10-17 17:35:20 +0200

    gérer l'égalité de 2 valeurs

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Valeur 1 : ");
         int valeur1 = scanner.nextInt();
+        System.out.print("Valeur 2 : ");
         int valeur2 = scanner.nextInt();
         if (valeur1 > valeur2) {
             System.out.println("La valeur la plus haute est : " + valeur1);
-        } else {
+        } else if (valeur1 < valeur2) {
             System.out.println("La valeur la plus haute est : " + valeur2);
+        } else {
+            System.out.println("Egalité");
         }
-
         scanner.close();
     }
 }
