d069c9c0e18ab42d9c4925f2cbb369260a095c44
Yahia BENDAHER <yahia.bendaher@hotmail.com>
2025-10-17 17:26:46 +0200

    1st slice : Comparer 2 valeurs sur l'entrée standard

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Main 1 : ");
-        String main1 = scanner.nextLine();
-        System.out.print(main1);
+        int valeur1 = scanner.nextInt();
+        int valeur2 = scanner.nextInt();
+        if (valeur1 > valeur2) {
+            System.out.println("La valeur la plus haute est : " + valeur1);
+        } else {
+            System.out.println("La valeur la plus haute est : " + valeur2);
+        }
+
+        scanner.close();
     }
 }
