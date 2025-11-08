ff7d07130ebdbeefc1a20b9f032062f77c525f97
Ahmed Belaid <elyesbelaid.2005@gmail.com>
2025-11-08 19:12:25 +0100

    Full Display fix

--- src/fr/pns/poker/evaluator/HandComparator.java
+++ src/fr/pns/poker/evaluator/HandComparator.java
@@ -66,6 +66,10 @@ public class HandComparator {
             case STRAIGHT_FLUSH:
                 Color couleur1 = eval.getColor();
                 return winner + " gagne avec un Quinte Flush, (Couleur: " + couleur1.getNomFrancais()+ ", Carte la plus haute : " + symbol + ")";
+            case FULL:
+                int lowValFull = values.get(1);
+                String lowSymbolFull = Value.getNameFromNumber(lowValFull);
+                return winner + " gagne avec un FULL, (Brelan de "+symbol+", Paire de "+lowSymbolFull+")";
             default:
                 return winner + " gagne (" + eval + ")";
         }
